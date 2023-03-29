45bfe007d8cb8607386bc30c0768507df4069068
3600X <wjq1933651291@163.com>
2023-03-30 02:30:55 +0800

    我完成了IQueue接口的描述

--- Lessons/src/week0106/IQueue.java
+++ Lessons/src/week0106/IQueue.java
@@ -1,5 +1,11 @@
 package week0106;
 
 public interface IQueue {
-    public void insert{};
+    public void clear();
+    public void isEmpty();
+    public int length();
+
+    public Object peek();
+    public void offer(Object x) throws Exception;
+    public Object poll();
 }
