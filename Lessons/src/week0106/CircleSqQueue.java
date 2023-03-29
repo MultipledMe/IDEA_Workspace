ff7d6bb235f37984f2aad0d0a2ad60ab695a6e9c
3600X <wjq1933651291@163.com>
2023-03-30 02:36:32 +0800

    我新建了一个循环顺序队列，麻烦写一下

---
+++ Lessons/src/week0106/CircleSqQueue.java
@@ -0,0 +1,37 @@
+package week0106;
+
+public class CircleSqQueue implements IQueue{
+    private Object [] queueElem;
+    private int front;
+    private int rear;
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void isEmpty() {
+
+    }
+
+    @Override
+    public int length() {
+        return 0;
+    }
+
+    @Override
+    public Object peek() {
+        return null;
+    }
+
+    @Override
+    public void offer(Object x) throws Exception {
+
+    }
+
+    @Override
+    public Object poll() {
+        return null;
+    }
+}
